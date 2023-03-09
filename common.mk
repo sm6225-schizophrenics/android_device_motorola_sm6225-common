7bf42a0445483095bb361d4f2b647d6317815aaf
Kunmun <kunmun@aospa.co>
2023-03-09 19:45:27 +0530

    sm6225-common: Makefile: Inherit hardware/motorola makefile
    Change-Id: I8631e13461bb3c5f9edd072124dcba01126df2f8

--- common.mk
+++ common.mk
@@ -367,6 +367,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+    
+# Hardware Motorola
+$(call inherit-product, $(HARDWARE_PATH)/common.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/motorola/sm6225-common/sm6225-common-vendor.mk)
