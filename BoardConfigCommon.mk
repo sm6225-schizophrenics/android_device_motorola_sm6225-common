04a19a763a6301862554610323d5e76617710bcd
Dyneteve <sound0020@gmail.com>
2023-08-27 21:56:20 -0300

    sm6225-common: Enable AOSP surfaceflinger * AOSP SurfaceFlinger is smoother than CAF

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -171,6 +171,9 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/motorola/sepolicy/qti/SEPolicy.mk
 
+# SurfaceFlinger
+TARGET_USE_AOSP_SURFACEFLINGER := true
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
