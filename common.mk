af02da294c1b8feda8860cafabf39d1137b2b8c7
Tommaso Fonda <fondatommaso2@gmail.com>
2024-04-06 10:01:24 +0200

    sm6225-common: Add MotoCamera to the privileged camera apps list
    (Partially) fixes Ultra-res mode.
    
    Change-Id: Id173eae5cee798291b8a735d7497b811d2046c08

--- common.mk
+++ common.mk
@@ -146,7 +146,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 PRODUCT_VENDOR_PROPERTIES += \
-    camera.disable_zsl_mode=1
+    camera.disable_zsl_mode=1 \
+    persist.vendor.camera.privapp.list=com.motorola.camera3
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
