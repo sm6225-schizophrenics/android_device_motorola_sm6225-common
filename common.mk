1bf3aa7e973fb3f35abfb1bd1ebeef62490705d8
Mashopy <eliasgheeraert@gmail.com>
2024-05-03 00:28:21 +0200

    fixup! sm6225-common: Add UClamp support for kona
    Forgot to add task_profiles.json from lahaina device tree
    
    Change-Id: Ib3571027e9678800e34b5241c5d14c89c1c03dd9

--- common.mk
+++ common.mk
@@ -243,6 +243,10 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
