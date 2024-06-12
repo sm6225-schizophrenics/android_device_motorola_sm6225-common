2767508abc02e5ad21edc80a8204e88be28f7310
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-12 22:41:36 +0800

    sm6225-common: Setup DeviceAsWebcam
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992

--- common.mk
+++ common.mk
@@ -260,6 +260,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+    DeviceAsWebcamResCommon \
     FrameworksResCommon \
     FrameworksResProduct \
     FrameworksResTarget \
