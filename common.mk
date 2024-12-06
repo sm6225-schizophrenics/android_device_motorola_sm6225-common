1bf59887b8e68ac83fb94ad4167c4b8f2b0ec687
P.Adarsh Reddy <padarshr@codeaurora.org>
2024-12-06 09:30:56 +0000

    sm6225-common: Enforce android-S new launch debugfs restrictions
    Change-Id: I78878e1c4177c07d66b13e5659d4360252c5f87b
    Signed-off-by: nnippon99 <adamayyad1999@hotmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- common.mk
+++ common.mk
@@ -114,6 +114,9 @@ PRODUCT_PACKAGES += \
 # ConfigStore
 PRODUCT_PACKAGES += \
     disable_configstore
+    
+# DebugFS
+PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
 # Display
 PRODUCT_PACKAGES += \
