69aaaf09fc1b2db90f8502fad593f4d7324daff3
LuK1337 <priv.luk@gmail.com>
2024-12-25 23:42:18 +0100

    sm6225-common: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643

--- bengal.mk
+++ bengal.mk
@@ -306,6 +306,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_PACKAGES += \
+    NcmTetheringOverlay
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
