9216f3ffcfcc6374567380cb1fd7e22bf9b6f5c9
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-19 02:44:05 +0200

    sm6225-common: switch to common QCOM AIDL bootctrl HAL
    Change-Id: I04ab771d3b1c38b58913607fbff1bb3b55e1fe25

--- bengal.mk
+++ bengal.mk
@@ -120,9 +120,8 @@ PRODUCT_PACKAGES += \
 
 # Bootctrl
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl-qti \
-    android.hardware.boot@1.2-impl-qti.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
 
  # Camera
 PRODUCT_COPY_FILES += \
