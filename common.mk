88f81d46452b5f27173cb8200595051cb3d54129
Iverz <asterixiverz@gmail.com>
2025-02-25 16:25:18 +0000

    sm6225-common: switch to Common QTI Bluetooth component
    Signed-off-by: Iverz <asterixiverz@gmail.com>

--- common.mk
+++ common.mk
@@ -97,9 +97,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor
 
 # Bootctrl
 PRODUCT_PACKAGES += \
@@ -302,6 +300,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     alarm \
     audio \
     av \
+    bt \
     display \
     gps \
     media \
