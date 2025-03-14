e3a8134b43953a01b92bf9373e37a8a73d1aed95
josexda <josegustavo6@protonmail.com>
2025-03-14 03:46:40 +0100

    sm6225-common: Inherit yaap common partition reserved sizes
    Change-Id: If1ab71032c92d934d48ff693b0277db261c48d45

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -118,7 +118,7 @@ TARGET_MODULE_ALIASES += \
 TARGET_USES_ION := true
 
 # Partitions
--include vendor/lineage/config/BoardConfigReservedSize.mk
+-include vendor/yaap/config/BoardConfigReservedSize.mk
 
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_USES_METADATA_PARTITION := true
