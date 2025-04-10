4c52813afc7338941757a9e6efc214e730bd31ed
4mede <4mede@tutamail.com>
2025-04-10 17:02:58 -0400

    sm6225-common: some fixes
    Change-Id: I48ae4029df4a059932e93e8b9c7099bc0cea226f

--- bengal.mk
+++ bengal.mk
@@ -126,9 +126,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
-    vendor.qti.hardware.memtrack-service \
-    libdisplaydebug \
-    libqservice
+    vendor.qti.hardware.memtrack-service
 
 PRODUCT_PACKAGES += \
     init.qti.display_boot.rc \
