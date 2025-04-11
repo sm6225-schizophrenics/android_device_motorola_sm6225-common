653fe9f38403fad805780ede12d435fe625924f4
4mede <4mede@tutamail.com>
2025-04-11 07:46:42 -0400

    sm6225-common: Add back device FCM
    Change-Id: Idb2c8a87ccbf31ed276af2a6312c26a90d49fa98

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -48,6 +48,7 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml \
     hardware/motorola/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     $(CUSTOM_PRODUCT_DIR)/config/device_framework_matrix.xml
