e3e7c82467b7c2626e70ff9eb3d6b963eecaa08b
4mede <4mede@tutamail.com>
2025-04-11 07:47:07 -0400

    sm6225-common: Fix building overlays
    Change-Id: I7c94882e76b44afe182ce60e69774455d1dc3285

--- bengal.mk
+++ bengal.mk
@@ -250,7 +250,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-custom
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
