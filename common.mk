c8d333fe3d7f3c52d629233a160c6173d9767101
4mede <4mede@tutamail.com>
2025-05-02 16:53:04 -0400

    sm6225-common: build prebuilt packages
    Change-Id: I29699836f97c5099e8daa11334d46a7f5ab8dbcb

--- common.mk
+++ common.mk
@@ -295,6 +295,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
+# Prebuilt
+$(call inherit-product, vendor/prebuilt/common.mk)
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
