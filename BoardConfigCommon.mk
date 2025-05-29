15d097b190adfe68a4f7b16e5083a326b87f68df
4mede <4mede@tutamail.com>
2025-05-29 10:31:42 -0400

    sm6225-common: drop device matrix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,7 +58,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
     vendor/pb/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest-qva.xml
 
 # Init
