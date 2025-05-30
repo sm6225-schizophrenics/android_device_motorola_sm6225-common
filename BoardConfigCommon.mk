450ede6db12b80e8cfd71b094cf6d3c8d34f09fc
4mede <4mede@tutamail.com>
2025-05-30 11:46:25 -0400

    Revert "sm6225-common: drop telephony packages"
    This reverts commit e353c474e48cbeb2308df4bed528b3250a824d27.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,6 +77,10 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_CONFIG := vendor/bengal-perf_defconfig vendor/debugfs.config vendor/ext_config/moto-bengal.config
 TARGET_KERNEL_SOURCE := kernel/motorola/sm6225
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := r416183b
+TARGET_KERNEL_CLANG_PATH := $(abspath .)/prebuilts/clang/kernel/$(HOST_PREBUILT_TAG)/clang-$(TARGET_KERNEL_CLANG_VERSION)
+TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
 
 # Kernel Modules - Audio
 TARGET_MODULE_ALIASES += \
