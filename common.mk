1eda19a41d14ddf9faf14ab3f54d6b3d0eb90db9
4mede <4mede@tutamail.com>
2025-06-18 15:35:13 -0400

    sm6225-common: set TARGET_DISABLE_EPPE to true

--- common.mk
+++ common.mk
@@ -394,5 +394,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
+# Evo
+TARGET_DISABLE_EPPE := true
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/motorola/sm6225-common/sm6225-common-vendor.mk)
