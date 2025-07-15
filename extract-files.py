e299bddc511b2c94f5f9968aea53d4a8502910b8
4mede <4mede@tutamail.com>
2025-07-15 19:44:16 -0400

    sm6225-common: Remove duplicate display blobs

--- extract-files.py
+++ extract-files.py
@@ -23,6 +23,8 @@ namespace_imports = [
     'vendor/qcom/opensource/dataservices',
     'vendor/qcom/common/system/display',
     "vendor/qcom/common/system/perf"
+    'vendor/qcom/common/vendor/gps-legacy',
+    'vendor/qcom/common/vendor/display/4.19'
 ]
 
 
