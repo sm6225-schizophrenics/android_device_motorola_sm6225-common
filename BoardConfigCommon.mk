57827c182404292f5e7899fad04d00eeb4b88adb
4mede <4mede@tutamail.com>
2025-07-15 20:47:36 -0400

    sm6225-common: Import device matrix from SunOS

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,7 +56,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/motorola/vintf/device_framework_matrix.xml \
     vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := device/qcom/common/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest-qva.xml
 
 # HWUI
