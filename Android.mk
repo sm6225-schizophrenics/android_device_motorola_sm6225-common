f2a73ab1dd3974fe5c99c0775248a2fd9d17029c
4mede <4mede@tutamail.com>
2025-07-15 20:38:32 -0400

    Revert "sm6225-common: Mark CNE symlink target as required package"
    This reverts commit a5093a707d650d85d2d75ac659b96474212a0eb7.

---
+++ Android.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2022-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter borneo capri caprip cebu guam guamna guamp devon hawao rhode, $(TARGET_DEVICE)),)
+include $(call all-makefiles-under, $(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+CNE_SYMLINKS := $(TARGET_OUT_VENDOR_APPS)/CneApp/lib/arm64/libvndfwk_detect_jni.qti.so
+$(CNE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "CneApp lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /vendor/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(CNE_SYMLINKS)
+
+endif
