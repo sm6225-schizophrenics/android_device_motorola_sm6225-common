84ffcbe911db59a093260edc3384f0e37ed633ff
Kunmun <kunmun@aospa.co>
2025-07-16 23:46:46 -0400

    sm6225-common: Fix up vintf

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,11 +58,10 @@ TARGET_GLOBAL_THINLTO := true
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/motorola/vintf/device_framework_matrix.xml \
     vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest-qva.xml
 
 # HWUI
